2c8f806605eccd44522083c32d26913ab0ca7583
Salman Mhaskar <salmanmhaskar17@gmail.com>
2026-06-10 20:16:09 +0400

    Initial Commit

---
+++ Rateify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rateify
+//
+//  Created by Salman Mhaskar on 10/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rateify/RateifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateifyApp.swift
+//  Rateify
+//
+//  Created by Salman Mhaskar on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RateifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
